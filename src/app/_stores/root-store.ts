1710d58f69a9827081d739a03058264c1e896326
Nill <niltonrck@gmail.com>
2024-10-12 05:32:31 -0300

    refactor: create a type for constructor params

--- src/app/_stores/root-store.ts
+++ src/app/_stores/root-store.ts
@@ -1,9 +1,13 @@
 import {WishlistStore} from '@/app/_stores/wishlist-store'
 import {ProductModel} from '@/types/models/products.model'
 
+type InitialValuesProps = {
+  wishlist?: ProductModel[]
+}
+
 export class RootStore {
   wishlistStore: WishlistStore
-  constructor({wishlist}: {wishlist: ProductModel[]}) {
+  constructor({wishlist}: InitialValuesProps) {
     this.wishlistStore = new WishlistStore(wishlist)
   }
 }
