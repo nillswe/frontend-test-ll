2119781eb6ebc2f4d475a489ef1bb6a31f225d58
Nill <niltonrck@gmail.com>
2024-10-12 04:41:14 -0300

    fix: wrong validation on add and remove to wishlist

--- src/app/_components/ui/product-card/_hooks/use-product-card.ts
+++ src/app/_components/ui/product-card/_hooks/use-product-card.ts
@@ -4,15 +4,13 @@ import {ProductModel} from '@/types/models/products.model'
 
 export const useProductCard = () => {
   const addProductToWishlist = (product: ProductModel) => {
-    const cachedProducts = localCache.get<ProductModel[]>(CACHE_WISHLIST)
-    if (!cachedProducts) return
+    const cachedProducts = localCache.get<ProductModel[]>(CACHE_WISHLIST) ?? []
 
     localCache.set(CACHE_WISHLIST, [...cachedProducts, product])
   }
 
   const removeProductFromWishlist = (product: ProductModel) => {
-    const cachedProducts = localCache.get<ProductModel[]>(CACHE_WISHLIST)
-    if (!cachedProducts) return
+    const cachedProducts = localCache.get<ProductModel[]>(CACHE_WISHLIST) ?? []
 
     const filteredProducts = cachedProducts.filter(({id}) => id !== product.id)
     localCache.set(CACHE_WISHLIST, filteredProducts)
--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,11 +1,40 @@
+'use client'
+
+import {Info} from 'lucide-react'
+import Link from 'next/link'
+
 import {ProductCard} from '@/app/_components/ui'
+import {CACHE_WISHLIST} from '@/config/tokens'
+import {localCache} from '@/services/local-cache/local-cache'
+import {ProductModel} from '@/types/models/products.model'
 
 export default function Page() {
+  const products = localCache.get<ProductModel[]>(CACHE_WISHLIST) ?? []
+
   return (
-    <section className='mb-10 mt-5 grid w-full grid-cols-4 gap-5'>
-      {Array.from({length: 3}).map((product, index) => {
-        return <ProductCard key={index} product={product as string} />
-      })}
-    </section>
+    <div className='mt-5  w-full'>
+      {products.length === 0 && (
+        <div className='flex w-full flex-col items-center gap-4'>
+          <p
+            role='alert'
+            className='mx-auto flex w-full max-w-3xl items-center gap-2 rounded-md bg-blue-300 p-4'>
+            <Info size={18} />
+            Sua lista de desejos está vazia. Comece adicionando algo agora mesmo!
+          </p>
+
+          <Link href='/' className='rounded-md bg-primary px-6 py-3 text-white'>
+            Ir para home page
+          </Link>
+        </div>
+      )}
+
+      {products.length > 0 && (
+        <section className='mb-10 grid w-full grid-cols-4 gap-5'>
+          {products.map((product, index) => {
+            return <ProductCard key={index} product={product} />
+          })}
+        </section>
+      )}
+    </div>
   )
 }
