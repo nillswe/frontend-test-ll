c65f86ce778a52e663fbed425188e42b12efb42c
Nill <niltonrck@gmail.com>
2024-10-12 03:40:08 -0300

    feat: add a comment

--- src/utils/currency.test.ts
+++ src/utils/currency.test.ts
@@ -6,6 +6,8 @@ describe('Currency utils', () => {
       const value = 100.99
       const sut = formatNumberToCurrency(value)
 
+      // needed to add a blank space using UTF8 char code %C2%A0 because
+      // Intl.NumberFormat add this character between the symbol and the number
       expect(sut).toBe(decodeURI('R$%C2%A0100,99'))
     })
   })
