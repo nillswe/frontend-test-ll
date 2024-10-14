5a4f4868de7a9b5ac2ca3c2c55a8726a892149f1
Nill <niltonrck@gmail.com>
2024-10-14 02:29:12 -0300

    test: ensure to render root layout properly

---
+++ src/app/layout.test.tsx
@@ -0,0 +1,29 @@
+import Layout from '@/app/layout'
+import {render, waitFor} from '@testing-library/react'
+
+const makeSut = () => {
+  const sut = render(
+    <Layout>
+      <div>children</div>
+    </Layout>,
+    {
+      container: document.body, // not recommended
+    },
+  )
+
+  return {
+    sut,
+  }
+}
+
+describe('Root Layout', () => {
+  it('Should ensure to render layout properly', async () => {
+    const {sut} = makeSut()
+
+    await waitFor(async () => {
+      const children = await sut.findByText('children')
+
+      expect(children).toBeVisible()
+    })
+  })
+})
