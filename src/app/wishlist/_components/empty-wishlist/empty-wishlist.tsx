85fe16476a9493085914abe94218a06291f6acd0
Nill <niltonrck@gmail.com>
2024-10-13 22:44:48 -0300

    feat: move empty list to its own component

---
+++ src/app/wishlist/_components/empty-wishlist/empty-wishlist.tsx
@@ -0,0 +1,19 @@
+import {Info} from 'lucide-react'
+import Link from 'next/link'
+
+export const EmptyWishlist = () => {
+  return (
+    <div className='flex w-full flex-col items-center gap-4'>
+      <p
+        role='alert'
+        className='mx-auto flex w-full max-w-3xl items-center gap-2 rounded-md bg-blue-300 p-4'>
+        <Info size={18} />
+        Sua lista de desejos está vazia. Comece adicionando algo agora mesmo!
+      </p>
+
+      <Link href='/' className='rounded-md bg-primary px-6 py-3 text-white'>
+        Ir para home page
+      </Link>
+    </div>
+  )
+}
--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import {Info} from 'lucide-react'
 import {observer} from 'mobx-react-lite'
-import Link from 'next/link'
 
 import {ProductsGrid, ProductCard} from '@/app/_components/ui'
 import {useRootStore} from '@/app/_hooks/use-root-store'
+import {EmptyWishlist} from '@/app/wishlist/_components'
 
 const Page = observer(() => {
   const {wishlistStore} = useRootStore()
@@ -13,20 +12,7 @@ const Page = observer(() => {
 
   return (
     <div className='mt-5 w-full'>
-      {products.length === 0 && (
-        <div className='flex w-full flex-col items-center gap-4'>
-          <p
-            role='alert'
-            className='mx-auto flex w-full max-w-3xl items-center gap-2 rounded-md bg-blue-300 p-4'>
-            <Info size={18} />
-            Sua lista de desejos está vazia. Comece adicionando algo agora mesmo!
-          </p>
-
-          <Link href='/' className='rounded-md bg-primary px-6 py-3 text-white'>
-            Ir para home page
-          </Link>
-        </div>
-      )}
+      {products.length === 0 && <EmptyWishlist />}
 
       {products.length > 0 && (
         <ProductsGrid>
