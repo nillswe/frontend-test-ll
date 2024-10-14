14375a95b093b347fc6b437b1dbbdef0de26bdd3
Nill <niltonrck@gmail.com>
2024-10-14 07:16:44 -0300

    feat: update productModel interface

--- src/mocks/product.mock.ts
+++ src/mocks/product.mock.ts
@@ -4,8 +4,10 @@ import {faker} from '@faker-js/faker'
 export const mockProduct = (): ProductModel => {
   return {
     id: faker.number.int(),
+    slug: faker.lorem.slug(),
     name: faker.commerce.productName(),
     pictureUrl: faker.image.urlPicsumPhotos(),
+    description: faker.commerce.productDescription(),
     rating: faker.helpers.arrayElement([0, 1, 1.3, 2, 2.5, 3, 3.6, 4, 4.7, 5]),
     oldPrice: faker.number.float({min: 99, multipleOf: 0.99, max: 600}),
     price: faker.number.float({min: 99, multipleOf: 2.5, max: 800}),
