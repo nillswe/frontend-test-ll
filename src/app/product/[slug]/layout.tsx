f44c46f8e8af773459306e28f214ad7aa9e8a832
Nill <niltonrck@gmail.com>
2024-10-14 23:59:13 -0300

    feat: display product name on page title

--- src/app/product/[slug]/layout.tsx
+++ src/app/product/[slug]/layout.tsx
@@ -1,9 +1,15 @@
 import type {Metadata} from 'next'
 import {PropsWithChildren} from 'react'
 
-export const metadata: Metadata = {
-  title: '[Fetch product name]',
-  description: 'Frontend app',
+import {getProductDetail} from '@/data/server/products.api'
+
+export const generateMetadata = async ({params}: {params: {slug: string}}): Promise<Metadata> => {
+  const product = await getProductDetail(params.slug)
+
+  return {
+    title: product?.name || 'Detalhe do produto',
+    description: product?.details_description,
+  }
 }
 
 export default function Layout({children}: PropsWithChildren) {
